8430d0fe509d1aefd05e1c733e7b4c31197f6076
SECRETARIA DE EDUCAÇÃO <sitec.seduc@gmail.com>
2025-10-14 18:18:30 +0000

    Correção React

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // ← Importa o Tailwind aqui
+import '../index.css' // ← Altere o caminho da importação aqui
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
